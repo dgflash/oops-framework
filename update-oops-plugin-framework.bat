19f589c901f3a342b87bb7d7dfebb4543280ae53
dgflash <dgflash@qq.com>
2022-09-23 21:54:48 +0800

    .

--- update-oops-plugin-framework.bat
+++ update-oops-plugin-framework.bat
@@ -8,7 +8,7 @@ goto clone
 )
 
 :clone
-git clone -b master https://gitee.com/dgflash/oops-plugin-framework.git
+git clone -b 3.6.0 https://gitee.com/dgflash/oops-plugin-framework.git
 
 :update
 cd oops-plugin-framework
