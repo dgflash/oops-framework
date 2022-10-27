1f3b28ed269804a11a8e654194e1929a0cdd7e67
dgflash <dgflash@qq.com>
2022-10-27 18:23:17 +0800

    .

--- assets/script/Main.ts
+++ assets/script/Main.ts
@@ -2,11 +2,10 @@
  * @Author: dgflash
  * @Date: 2021-07-03 16:13:17
  * @LastEditors: dgflash
- * @LastEditTime: 2022-10-14 15:25:22
+ * @LastEditTime: 2022-10-27 18:23:06
  */
 import { dynamicAtlasManager, macro, profiler, _decorator } from 'cc';
 import { DEBUG, JSB } from 'cc/env';
-import { RandomManager } from '../../extensions/oops-plugin-framework/assets/core/common/manager/RandomManager';
 import { oops } from '../../extensions/oops-plugin-framework/assets/core/Oops';
 import { Root } from '../../extensions/oops-plugin-framework/assets/core/Root';
 import { ecs } from '../../extensions/oops-plugin-framework/assets/libs/ecs/ECS';
@@ -27,10 +26,10 @@ dynamicAtlasManager.maxFrameSize = 512;
 export class Main extends Root {
     start() {
         if (DEBUG) profiler.showStats();
-        RandomManager.instance.setSeed(1);
-        for (let index = 0; index < 10; index++) {
-            console.log(RandomManager.instance.getRandomInt(1, 1000));
-        }
+        // RandomManager.instance.setSeed(1);
+        // for (let index = 0; index < 10; index++) {
+        //     console.log(RandomManager.instance.getRandomInt(1, 1000));
+        // }
     }
 
     protected run() {
