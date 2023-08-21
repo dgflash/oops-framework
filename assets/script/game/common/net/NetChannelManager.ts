d1abd35d01d167585ed8498275f81656fe07b3c5
dgflash <dgflash@qq.com>
2023-08-21 15:26:56 +0800

    抛出框架中的随机数工具oops.random与长连接库oops.tcp

--- assets/script/game/common/net/NetChannelManager.ts
+++ assets/script/game/common/net/NetChannelManager.ts
@@ -3,8 +3,8 @@
  * @LastEditors: dgflash
  * @LastEditTime: 2022-06-14 17:53:02
  */
+import { oops } from "../../../../../extensions/oops-plugin-framework/assets/core/Oops";
 import { NetData } from "../../../../../extensions/oops-plugin-framework/assets/libs/network/NetInterface";
-import { NetManager } from "../../../../../extensions/oops-plugin-framework/assets/libs/network/NetManager";
 import { NetProtocolPako } from "../../../../../extensions/oops-plugin-framework/assets/libs/network/NetProtocolPako";
 import { WebSock } from "../../../../../extensions/oops-plugin-framework/assets/libs/network/WebSock";
 import { netConfig } from "./NetConfig";
@@ -17,7 +17,7 @@ export enum NetChannelType {
 }
 
 /** 游戏服务器心跳协议 */
-class GameProtocol extends NetProtocolPako { 
+class GameProtocol extends NetProtocolPako {
     /** 心跳协议 */
     getHearbeat(): NetData {
         return `{"action":"LoginAction","method":"heart","data":"null","isCompress":false,"channelid":${netConfig.channelid},"callback":"LoginAction_heart"}`;
@@ -32,12 +32,12 @@ export class NetChannelManager {
         this.game = new NetNodeGame();
         // 游戏网络事件逻辑统一在 NetGameTips 里写
         this.game.init(new WebSock(), new GameProtocol(), new NetGameTips());
-        NetManager.getInstance().setNetNode(this.game, NetChannelType.Game);
+        oops.tcp.setNetNode(this.game, NetChannelType.Game);
     }
 
     /** 连接游戏服务器 */
     gameConnect() {
-        NetManager.getInstance().connect({
+        oops.tcp.connect({
             url: `ws://${netConfig.gameIp}:${netConfig.gamePort}`,
             autoReconnect: 0        // 手动重连接
         }, NetChannelType.Game);
@@ -45,7 +45,7 @@ export class NetChannelManager {
 
     /** 断开游戏服务器 */
     gameClose() {
-        NetManager.getInstance().close(undefined, undefined, NetChannelType.Game);
+        oops.tcp.close(undefined, undefined, NetChannelType.Game);
     }
 }
 
